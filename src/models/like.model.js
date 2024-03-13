e2efac30640dc46302c2cb19000e00dcb52e10d7
Varun <varundixit1906@gmail.com>
2024-03-13 13:09:22 +0530

    Added commnet, like, playlist and tweet models and also solved bug of logOut in user.controller.js

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import mongoose, {Schema} from mongoose;
+
+const likeSchema = new Schema(
+    {
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        }, 
+
+        tweet: {
+            type: Schema.Types.ObjectId,
+            ref: "tweet"
+        },
+
+        likedBY: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Like = mongoose.model("Like", likeSchema)
